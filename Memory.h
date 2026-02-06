1699ed5a0639b2256259bb5082af85902e2ea9f0
matin <matin.akbari06ap@gmail.com>
2026-02-06 13:32:47 +0330

    add Memory for cache and ram

--- Memory.h
+++ Memory.h
@@ -4,9 +4,18 @@
 
 #ifndef MEMORY_ARCHITECTURE_MEMORY_H
 #define MEMORY_ARCHITECTURE_MEMORY_H
+#include <cstdint>
 
+using namespace std;
 
 class Memory {
+protected:
+    uint64_t size;
+    int baseDelay;
+public:
+    virtual bool getData(uint64_t address) = 0;
+    virtual int getDelay() = 0;
+    virtual void replace(uint64_t address) = 0;
 };
 
 
